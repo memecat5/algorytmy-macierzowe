600b3e5a4b72cf8e0dd9ef2d6f0e5247d1672de5
memecat5 <tomaszglabek55@gmail.com>
2025-12-08 15:42:08 +0100

    random messy fixes and plots

--- lab1/include/multiply_strassen.hpp
+++ lab1/include/multiply_strassen.hpp
@@ -4,5 +4,12 @@
 
 using Matrix = std::vector<std::vector<double>>;
 
+// Struktura do przechowywania statystyk
+struct StrassenStats {
+    long long additions = 0;       // Licznik dodawań (i odejmowań)
+    long long multiplications = 0; // Licznik mnożeń
+};
+
 Matrix createMatrix(size_t rows, size_t cols, double value = 0.);
 Matrix multiplyStrassen(const Matrix& A, const Matrix& B);
+Matrix multiplyStrassen(const Matrix& A, const Matrix& B, StrassenStats& stats);
